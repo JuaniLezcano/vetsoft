47cdafee0a327fe84b9dd65d583357cedac8cf5a
Valentin Peres <peresvalentin09@gmail.com>
2024-06-04 15:58:36 -0300

    [feat]: agrego nuevo test y su dockstring

--- app/tests_integration.py
+++ app/tests_integration.py
@@ -68,6 +68,21 @@ class ClientsTest(TestCase):
             },
         )
         self.assertContains(response, "Por favor ingrese un email valido")
+    
+    def test_validation_format_vet(self):
+        """
+        Valido que el formato sea el correcto, en caso de no serlo da un error
+        """
+        response = self.client.post(
+            reverse("clients_form"),
+            data={
+                "name": "Juan Sebastian Veron",
+                "phone": "221555232",
+                "address": "13 y 44",
+                "email": "brujita75@gmail.com",
+            },
+        )
+        self.assertContains(response, "Por favor ingrese un email valido")
 
     def test_edit_user_with_valid_data(self):
         client = Client.objects.create(
